aa22b266936a37543f41cc3528cbfc959589eeda
harbin19 <54925144+harbin19@users.noreply.github.com>
2022-06-09 08:19:31 +0800

    Update testa.js

--- testa.js
+++ testa.js
@@ -1,10 +1,10 @@
 var randstr='',ticket='',secretId='',address='',interval='',vcon='',htmm='',scri='',timeout=555;
 var A={
-        activityInfoId:'b984613988d64c82ba0d347633dcc98a',
+        activityInfoId:'1c786bd21de5477485e7739e3be5126c',
         payType:'WX_PAY',
         redirectUrl:'https://club.10155.com/shop/orderDetail',
-        spuId:'10005315',
-        id:'dc93b49b999148fc8bf44dc36132bf83'
+        spuId:'10005961',
+        id:'fbad1895593c40328b5b1cdf6d009b12'
     };
 var AA={
         activityInfoId:'b984613988d64c82ba0d347633dcc98a',
