171b0803223f94a946ec965002a593dfd69bd156
harbin19 <54925144+harbin19@users.noreply.github.com>
2022-07-10 15:55:43 +0800

    Create data_testb.js

---
+++ data_testb.js
@@ -0,0 +1,87 @@
+var oo=[
+    {"title":"纳朝 200*7=1400",
+    "id":{
+  "orderType": 1,
+  "proProType1":3,
+  "orgId": "727551618916024320",
+  "shipMode": 1,
+  "orderDetailList": [
+    {
+      "ostId": "",
+      "proId": "930827192835768320",
+      "quantity": 7
+    }
+  ]
+}},
+    {"title":"金箫 175*8=1400",
+    "id":{
+  "orderType": 1,
+  "proProType1":3,
+  "orgId": "727551618916024320",
+  "shipMode": 1,
+  "orderDetailList": [
+    {
+      "ostId": "",
+      "proId": "915347481921847296",
+      "quantity": 8
+    }
+  ]
+}},
+    {"title":"鲜宴 712*2=1424",
+    "id":{
+  "orderType": 1,
+  "proProType1":3,
+  "orgId": "727551618916024320",
+  "shipMode": 1,
+  "orderDetailList": [
+    {
+      "ostId": "",
+      "proId": "930781335004381184",
+      "quantity": 2
+    }
+  ]
+}},
+    {"title":"陆珍鲜 178*8=1424",
+    "id":{
+  "orderType": 1,
+  "proProType1":3,
+  "orgId": "727551618916024320",
+  "shipMode": 1,
+  "orderDetailList": [
+    {
+      "ostId": "",
+      "proId": "930836059288240128",
+      "quantity": 8
+    }
+  ]
+}},
+    {"title":"油研 200*7=1400",
+    "id":{
+  "orderType": 1,
+  "proProType1":3,
+  "orgId": "727551618916024320",
+  "shipMode": 1,
+  "orderDetailList": [
+    {
+      "ostId": "",
+      "proId": "915348730478067712",
+      "quantity": 7
+    }
+  ]
+}},
+    {"title":"黔民 175*8=1400",
+    "id":{
+  "orderType": 1,
+  "proProType1":3,
+  "orgId": "727551618916024320",
+  "shipMode": 1,
+  "orderDetailList": [
+    {
+      "ostId": "",
+      "proId": "915348593747951616",
+      "quantity": 8
+    }
+  ]
+}}
+];
+var o=oo[3].id;
