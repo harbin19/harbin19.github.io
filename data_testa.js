1487b24def6f854a6e5686e2a552682dd3b1475a
harbin19 <54925144+harbin19@users.noreply.github.com>
2022-07-10 11:48:50 +0800

    Update data_testa.js

--- data_testa.js
+++ data_testa.js
@@ -1,21 +1,15 @@
 var A={
         activityInfoId:'f4d785fd075840f6b4fd580f21e39a3d',
-        payType:'WX_PAY',
-        redirectUrl:'https://club.10155.com/shop/orderDetail',
         spuId:'10006066',
         id:'f5c37f9ba65343509a0b51c2da06f4d8'
     };
 var AA={
         activityInfoId:'f4d785fd075840f6b4fd580f21e39a3d',
-        payType:'WX_PAY',
-        redirectUrl:'https://club.10155.com/shop/orderDetail',
         spuId:'10006550',
         id:'79d2a1e37a9c436e822ce7a4d2500f04'
     };
 var AAA={
         activityInfoId:'b466d542fcb5448a8a81087673216b16',
-        payType:'WX_PAY',
-        redirectUrl:'https://club.10155.com/shop/orderDetail',
         spuId:'10003868',
         id:'fd30c2c6f7da4d23a95ea3124c9c3ce8'
     };
